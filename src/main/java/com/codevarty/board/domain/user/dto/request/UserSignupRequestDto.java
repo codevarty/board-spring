3c8756b548c378a46fc0e7d2102fd01519280c68
hisu7 <hisu7@118.91.0.195>
2026-03-01 16:39:35 +0900

    feat : 사용자 비밀번호, 이메일 검증 추가

--- src/main/java/com/codevarty/board/domain/user/dto/request/UserSignupRequestDto.java
+++ src/main/java/com/codevarty/board/domain/user/dto/request/UserSignupRequestDto.java
@@ -1,6 +1,7 @@
 package com.codevarty.board.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +20,10 @@ public class UserSignupRequestDto {
 	private String username; // 사용자명
 	
 	@NotBlank(message = "비밀번호는 필수 값입니다.")
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password; // 비밀번호
 	
+	@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
 	@NotBlank(message = "이메일은 필수 값입니다.")
 	private String email; // 이메일
 	
